42409c5fdb2a2d5a47941f0fa45c3a43926b5cc3
fhabhishektiwari <abhishektiwarifh@gmail.com>
2023-11-04 23:28:23 +0530

    Added more pattern

--- pattern.py
+++ pattern.py
@@ -110,12 +110,49 @@
 4 4 4 4
 """
 
-n=int(input("Enter a number: "))
-i=1
-while i<=n:
-    j=1
-    while j<=i:
-        print(i,end=" ")
-        j=j+1
-    print("")
-    i=i+1
+# n=int(input("Enter a number: "))
+# i=1
+# while i<=n:
+#     j=1
+#     while j<=i:
+#         print(i,end=" ")
+#         j=j+1
+#     print("")
+#     i=i+1
+
+# =================================================
+"""
+1
+2 3
+4 5 6
+7 8 9 10
+"""
+# n=int(input("Enter a number: "))
+# row = current_number = 1
+# while row<=n:
+#     col=1
+#     while col<=row:
+#         print(current_number,end=" ")
+#         current_number+=1
+#         col=col+1
+#     print("")
+#     row=row+1
+
+# =================================================
+"""
+1
+2 3
+3 4 5
+4 5 6 7
+"""
+# num_of_row = int(input("Enter a number: "))
+# row = 1
+# while row<=num_of_row:
+#     col=1
+#     current_value = row
+#     while col<=row:
+#         print(current_value,end=" ")
+#         current_value+=1
+#         col=col+1
+#     print("")
+#     row=row+1
