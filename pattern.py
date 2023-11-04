0f43d45084ddeefd328a51b3b8b41b08d1c143f9
fhabhishektiwari <abhishektiwarifh@gmail.com>
2023-11-04 23:04:05 +0530

    Added more patterns

--- pattern.py
+++ pattern.py
@@ -67,3 +67,55 @@
 #         j=j+1
 #     print("")
 #     i=i+1
+
+"""
+1 2 3
+4 5 6 
+7 8 9
+"""
+# n=int(input("Enter a number: "))
+# i=1
+# count=1
+# while i<=n:
+#     j=1
+#     while j<=n:
+#         print(count,end=" ")
+#         count+=1
+#         j=j+1
+#     print("")
+#     i=i+1
+
+# ====================================================
+"""
+*
+* *
+* * *
+* * * *
+"""
+# n=int(input("Enter a number: "))
+# i=1
+# while i<=n:
+#     j=1
+#     while j<=i:
+#         print("*",end=" ")
+#         j=j+1
+#     print("")
+#     i=i+1
+
+# =========================================================
+"""
+1
+2 2
+3 3 3
+4 4 4 4
+"""
+
+n=int(input("Enter a number: "))
+i=1
+while i<=n:
+    j=1
+    while j<=i:
+        print(i,end=" ")
+        j=j+1
+    print("")
+    i=i+1
